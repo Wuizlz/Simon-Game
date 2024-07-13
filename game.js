58fa09a79b3c6e4bb56ce20822bb2fca6895f346
Daniel <Brisenodan12@gmail.com>
2024-07-13 11:23:32 -0500

    Updated game logic and added level display

--- game.js
+++ game.js
@@ -1,38 +1,40 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedButton = [];
+var level = 0;
 
-function nextSequence()
-{
-    var randomNum = Math.floor(Math.random() * 4 );
-    var randomChosenColor = buttonColors[randomNum];
-    gamePattern.push(randomChosenColor);
-    $(`#${randomChosenColor}`).addClass("pressed");
-   
-    setTimeout(() =>
-    {
-        $(`#${randomChosenColor}`).removeClass("pressed");
-    }, 100);
-    playSound(randomChosenColor);
+function nextSequence() {
+  level++;
+  $("#level-title").text(`Level ${level}`)
+  var randomNum = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNum];
+  gamePattern.push(randomChosenColor);
+  $(`#${randomChosenColor}`).addClass("pressed");
+
+  setTimeout(() => {
+    $(`#${randomChosenColor}`).removeClass("pressed");
+  }, 100);
+  playSound(randomChosenColor);
 }
+$(document).on("keydown", () => {
+  if (level == 0) {
+    nextSequence();
+  }
+});
+$(document).ready(function () {
+  $(document).on("click", "[id]", function () {
+    var userChosenColor = this.id;
+    userClickedButton.push(userChosenColor);
+    playSound(userChosenColor);
+    $(`#${userChosenColor}`).addClass("pressed");
 
-$(document).ready(function() {
-    $(document).on('click', '[id]', function() {
-        var userChosenColor = this.id;
-        userClickedButton.push(userChosenColor);
-        playSound(userChosenColor);
-        $(`#${userChosenColor}`).addClass("pressed");
-
-        setTimeout(() =>
-        {
-            $(`#${userChosenColor}`).removeClass("pressed");
-        }, 100);
-    })
+    setTimeout(() => {
+      $(`#${userChosenColor}`).removeClass("pressed");
+    }, 100);
+  });
 });
 
-
-function playSound(name)
-{
-    var audio = new Audio(`./sounds/${name}.mp3`);
-    audio.play();
+function playSound(name) {
+  var audio = new Audio(`./sounds/${name}.mp3`);
+  audio.play();
 }
