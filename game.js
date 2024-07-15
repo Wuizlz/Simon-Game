1b97564f131f7e4a4aff3b4138877e099956ca9c
Daniel Briseno <wuzi@daniels-laptop.mynetworksettings.com>
2024-07-15 12:27:09 -0500

    Finished Game

--- game.js
+++ game.js
@@ -2,8 +2,13 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedButton = [];
 var level = 0;
-
+$(document).on("keydown", () => {
+  if (level == 0) {
+    nextSequence();
+  }
+});
 function nextSequence() {
+  userClickedButton = [];
   level++;
   $("#level-title").text(`Level ${level}`)
   var randomNum = Math.floor(Math.random() * 4);
@@ -16,13 +21,8 @@ function nextSequence() {
   }, 100);
   playSound(randomChosenColor);
 }
-$(document).on("keydown", () => {
-  if (level == 0) {
-    nextSequence();
-  }
-});
-$(document).ready(function () {
-  $(document).on("click", "[id]", function () {
+
+  $(document).on("click", ".btn", function () {
     var userChosenColor = this.id;
     userClickedButton.push(userChosenColor);
     playSound(userChosenColor);
@@ -31,10 +31,43 @@ $(document).ready(function () {
     setTimeout(() => {
       $(`#${userChosenColor}`).removeClass("pressed");
     }, 100);
+    checkAnswer(userClickedButton.length - 1);
   });
-});
+  
 
 function playSound(name) {
   var audio = new Audio(`./sounds/${name}.mp3`);
   audio.play();
+}
+
+function checkAnswer(currentLevel)
+{
+  if(userClickedButton[currentLevel] === gamePattern[currentLevel])
+  {
+    if(userClickedButton.length === gamePattern.length)
+    {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else
+  {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(()=>
+    {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver()
+{
+  userClickedButton = [];
+  gamePattern = [];
+  level = 0;
+
 }
